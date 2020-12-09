725396b321ec47e4c7fe3071131f3e34d6d0f9fa
George Svitenko <georg.svit@gmail.com>
2020-12-09 15:23:18 +0200

    Admin`s registration and login methods implemented

--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -1,38 +1,90 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:smart_dripper_frontend/DTO/Responses/admin_response.dart';
 import 'package:smart_dripper_frontend/DTO/Responses/doctor_response.dart';
 import 'package:http/http.dart' as http;
 import 'package:smart_dripper_frontend/DTO/routes.dart';
+import 'package:smart_dripper_frontend/utils/session.dart';
 
 Future<DoctorResponse> loginDoctor(String login, String password) async {
   final http.Response response = await http.post(
     Routes.doctorsLogin,
     headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
+      HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8',
     },
     body: jsonEncode(<String, dynamic>{'Login': login, 'Password': password}),
   );
   if (response.statusCode == 200) {
-    print(response.body);
     var doctor = DoctorResponse.fromJson(jsonDecode(response.body));
-    print(doctor.token);
+    saveToken(doctor.token);
     return doctor;
   } else {
     throw Exception('Failed to login.');
   }
 }
 
-Future registerDoctor(String login, String password, String name, String surname) async {
+Future registerDoctor(
+    String login, String password, String name, String surname) async {
   final http.Response response = await http.post(
     Routes.doctorsRegister,
     headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
+      HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8',
     },
-    body: jsonEncode(<String, dynamic>{'Login': login, 'Password': password, 'Name': name, 'Surname': surname}),
+    body: jsonEncode(<String, dynamic>{
+      'Login': login,
+      'Password': password,
+      'Name': name,
+      'Surname': surname
+    }),
   );
   if (response.statusCode == 200) {
-    print(response.body);
+    print(response);
   } else {
     throw Exception('Failed to login.');
+  }
+}
+
+Future<AdminResponse> loginAdmin(String login, String password) async {
+  final http.Response response = await http.post(
+    Routes.adminsLogin,
+    headers: <String, String>{
+      HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, dynamic>{'Login': login, 'Password': password}),
+  );
+  if (response.statusCode == 200) {
+    var admin = AdminResponse.fromJson(jsonDecode(response.body));
+    saveToken(admin.token);
+    return admin;
+  } else {
+    throw Exception('Failed to login.');
+  }
+}
+
+Future registerAdmin(String login, String password, String name, String surname) async {
+  final token = await fetchToken();
+
+  if (token != "") {
+    final http.Response response = await http.post(
+      Routes.adminsRegister,
+      headers: <String, String>{
+        HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8',
+        HttpHeaders.authorizationHeader: 'Bearer $token'
+      },
+      body: jsonEncode(<String, dynamic>{
+        'Login': login,
+        'Password': password,
+        'Name': name,
+        'Surname': surname
+      }),
+    );
+    if (response.statusCode == 200) {
+      print(response);
+    } else {
+      throw Exception('Failed to login.');
+    }
+  } else {
+    throw Exception('Token is null.');
   }
 }

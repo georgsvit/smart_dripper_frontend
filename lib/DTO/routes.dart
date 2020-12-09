854142578bfda12ad17493ac929dce1aa2c5776e
George Svitenko <georg.svit@gmail.com>
2020-12-09 11:00:32 +0200

    Base routes configured

---
+++ lib/DTO/routes.dart
@@ -0,0 +1,13 @@
+class Routes {
+
+  static const String baseURL = "https://localhost:44354/";
+  static const String admins = baseURL + "admins/";
+  static const String nurses = baseURL + "nurses/";
+  static const String doctors = baseURL + "doctors/";
+
+
+  static const String doctorsLogin = doctors + "login";
+  static const String doctorsRegister = doctors + "register";
+
+
+}

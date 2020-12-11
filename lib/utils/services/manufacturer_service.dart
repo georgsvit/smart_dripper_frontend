aeaae868950eb2e6cd8bb1d7a728079df73faabf
George Svitenko <georg.svit@gmail.com>
2020-12-11 18:13:16 +0200

    Mnufacturer service (CRUD) implemented

---
+++ lib/utils/services/manufacturer_service.dart
@@ -0,0 +1,129 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:smart_dripper_frontend/dto/Responses/manufacturer_response.dart';
+import 'package:smart_dripper_frontend/dto/routes.dart';
+import 'package:smart_dripper_frontend/utils/api_status.dart';
+import 'package:smart_dripper_frontend/utils/session.dart';
+
+Future<List<ManufacturerResponse>> getAllManufacturers() async {
+  final token = await fetchToken();
+  
+  final http.Response response = await http.get(
+    Routes.manufacturersGetAll,
+    headers: <String, String>{
+      HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8',
+      HttpHeaders.authorizationHeader: 'Bearer $token'
+    },
+  );
+  if (response.statusCode == 200) {
+    var list = (json.decode(response.body) as List).map((data) => ManufacturerResponse.fromJson(data)).toList();
+
+    return list;
+  } else {
+    throw Exception(jsonDecode(response.body)['message']);
+  }
+}
+
+Future<ManufacturerResponse> getManufacturer(String id) async {
+  final token = await fetchToken();
+
+  var uri = Uri.encodeFull(Routes.manufacturersGet + id);
+
+  final http.Response response = await http.get(
+    uri,
+    headers: <String, String>{
+      HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8',
+      HttpHeaders.authorizationHeader: 'Bearer $token'
+    },
+  );
+  if (response.statusCode == 200) {
+
+    print(response.body);
+
+    var element = ManufacturerResponse.fromJson(json.decode(response.body));
+
+    return element;
+  } else {
+    if (response.statusCode > 400) {
+      throw Exception("Unauthorized");
+    } else {
+      print(response.body);
+      throw Exception(jsonDecode(response.body)['errors']['id']);
+    }
+  }
+}
+
+Future<ApiStatus> createManufacturer(ManufacturerResponse manufacturer) async {
+  final token = await fetchToken();
+
+  var uri = Uri.encodeFull(Routes.manufacturersCreate);
+
+  final http.Response response = await http.post(
+    uri,
+    headers: <String, String>{
+      HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8',
+      HttpHeaders.authorizationHeader: 'Bearer $token'
+    },
+    body: jsonEncode(<String, dynamic>{'Name': manufacturer.name, 'Country': manufacturer.country})
+  );
+  if (response.statusCode == 200) {
+    return ApiStatus.success;
+  } else {
+    if (response.statusCode > 400) {
+      throw Exception("Unauthorized");
+    } else {
+      throw Exception(response.body);
+    }
+  }
+}
+
+Future<ApiStatus> deleteManufacturer(String id) async {
+  final token = await fetchToken();
+
+  var uri = Uri.encodeFull(Routes.manufacturersDelete + id);
+
+  final http.Response response = await http.delete(
+    uri,
+    headers: <String, String>{
+      HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8',
+      HttpHeaders.authorizationHeader: 'Bearer $token',
+    },
+  );
+  if (response.statusCode == 200) {
+    return ApiStatus.success;
+  } else {
+    if (response.statusCode > 400) {
+      throw Exception("Unauthorized");
+    } else {
+      throw Exception(response.body);
+    }
+  }
+}
+
+Future<ManufacturerResponse> editManufacturer(String id, ManufacturerResponse manufacturer) async {
+  final token = await fetchToken();
+
+  var uri = Uri.encodeFull(Routes.manufacturersEdit + id);
+
+  final http.Response response = await http.patch(
+    uri,
+    headers: <String, String>{
+      HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8',
+      HttpHeaders.authorizationHeader: 'Bearer $token'
+    },
+    body: jsonEncode(<String, dynamic>{'Name': manufacturer.name, 'Country': manufacturer.country})
+  );
+  if (response.statusCode == 200) {
+    print(ManufacturerResponse.fromJson(json.decode(response.body)));
+    return ManufacturerResponse.fromJson(json.decode(response.body));
+  } else {
+    print(response.statusCode);
+    if (response.statusCode > 400) {
+      throw Exception("Unauthorized");
+    } else {
+      throw Exception(jsonDecode(response.body)['errors']['id']);
+    }
+  }
+}

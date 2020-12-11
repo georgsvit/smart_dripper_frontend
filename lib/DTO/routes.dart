6a1a00d9abc8ef8d03451f8bb8714266f5351f04
George Svitenko <georg.svit@gmail.com>
2020-12-11 18:12:24 +0200

    Manufacturer`s routes added

--- lib/DTO/routes.dart
+++ lib/DTO/routes.dart
@@ -2,13 +2,19 @@ class Routes {
 
   static const String baseURL = "https://localhost:44354/";
   static const String admins = baseURL + "admins/";
-  static const String nurses = baseURL + "nurses/";
   static const String doctors = baseURL + "doctors/";
-
+  static const String manufacturers = baseURL + 'manufacturers/';
 
   static const String doctorsLogin = doctors + "login";
   static const String doctorsRegister = doctors + "register";
   static const String adminsLogin = admins + "login";
   static const String adminsRegister = admins + "register";
+
+  // Manufacturers
+  static const String manufacturersGetAll = manufacturers;
+  static const String manufacturersGet = manufacturers;
+  static const String manufacturersDelete = manufacturers + "delete/";
+  static const String manufacturersEdit = manufacturers + "edit/";
+  static const String manufacturersCreate = manufacturers + "create";
 
 }

4982aea86be4979c7ec508396eaa2450c23f18c5
George Svitenko <georg.svit@gmail.com>
2020-12-11 18:12:45 +0200

    Manufacturer DTO created

---
+++ lib/DTO/Responses/manufacturer_response.dart
@@ -0,0 +1,11 @@
+class ManufacturerResponse {
+  final String id;
+  final String name;
+  final String country;
+
+  ManufacturerResponse(this.id, this.name, this.country);
+
+  factory ManufacturerResponse.fromJson(Map<String, dynamic> json) {
+    return new ManufacturerResponse(json['id'], json['name'], json['country']);
+  }
+}
